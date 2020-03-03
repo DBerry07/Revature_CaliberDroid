b51ab93fc6c1020241ffe52402d138c86deadcf1
gmitch2 <gamitchell4@gmail.com>
2020-03-03 16:19:41 -0500

    minor change

--- app/src/main/java/com/revature/caliberdroid/ui/AssessBatchTraineeRecyclerAdapter.kt
+++ app/src/main/java/com/revature/caliberdroid/ui/AssessBatchTraineeRecyclerAdapter.kt
@@ -9,15 +9,6 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.github.wrdlbrnft.sortedlistadapter.SortedListAdapter
 import com.revature.caliberdroid.data.model.Trainee
 
-class AssessBatchTraineeRecyclerAdapter() : SortedListAdapter<SortedListAdapter.ViewModel>() {
-    private var items : List<Trainee> = ArrayList()
-
-    override fun onCreateViewHolder(
-        inflater: LayoutInflater,
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewHolder<out SortedListAdapter.ViewModel> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class AssessBatchTraineeRecyclerAdapter(){
 
 }

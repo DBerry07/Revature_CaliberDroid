339e1cc24591a579d9f4119477835aa138b982ba
Phirom Peterschmidt <phiromp@gmail.com>
2020-03-05 16:38:05 -0500

    date converter class for the timestamp, implemented in batch info screen

---
+++ app/src/main/java/com/revature/caliberdroid/util/DateConverter.kt
@@ -0,0 +1,12 @@
+package com.revature.caliberdroid.util
+import java.text.DateFormat.getDateInstance
+import java.util.*
+
+object DateConverter {
+    fun getDate(timestamp: Long): String {
+//        return DateTimeFormatter.ISO_DATE.format(Instant.ofEpochMilli(timestamp))
+        val sdf = getDateInstance()
+        val netDate = Date(timestamp)
+        return sdf.format(netDate)
+    }
+}

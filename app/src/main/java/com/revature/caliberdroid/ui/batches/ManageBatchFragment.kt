3a06551a50ca5206be77219bb76f447fb16803a0
Phirom Peterschmidt <phiromp@gmail.com>
2020-03-05 15:26:37 -0500

    Updated the Batch Adapter, implemented data binding, filled manage batch with data, filled batch info with selected batch data

--- app/src/main/java/com/revature/caliberdroid/ui/batches/ManageBatchFragment.kt
+++ app/src/main/java/com/revature/caliberdroid/ui/batches/ManageBatchFragment.kt
@@ -11,24 +11,16 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.revature.caliberdroid.R
-import com.revature.caliberdroid.adapter.batches.BatchesAdapter
-import com.revature.caliberdroid.data.api.APIHandler.context
 import com.revature.caliberdroid.data.model.Batch
-import com.revature.caliberdroid.ui.batches.BatchesViewModel
 import com.revature.caliberdroid.databinding.FragmentBatchesBinding
+import com.revature.caliberdroid.ui.batches.BatchAdapter.OnItemClickListener
 import java.util.*
-import kotlin.collections.ArrayList
 
 
-class ManageBatchFragment : Fragment() {
-
-    private var temp = ArrayList(Arrays.asList("", "", "", "", "", ""))
+class ManageBatchFragment : Fragment(), OnItemClickListener {
 
     private var _binding: FragmentBatchesBinding? = null
     private val binding
@@ -47,30 +39,17 @@ class ManageBatchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentBatchesBinding.inflate(layoutInflater)
+
+        binding.recyclerviewManageBatches.layoutManager = LinearLayoutManager(activity)
+        binding.recyclerviewManageBatches.adapter = BatchAdapter(requireContext(), ALPHABETICAL_COMPARATOR_BATCHES, this)
+
         viewModel.getBatches()
-        binding.apply {
-            // New way of inserting the data into the ui directly from the view model, all changes are automatically updated on the UI
-            // batchesViewModel = viewModel
+        subscribeToViewModel()
 
-            // Make sure to call this method so the UI reflect the changes on the view model
-            // setLifecycleOwner(this@BatchesFragment)
-            viewModel.batchesLiveData.observe(viewLifecycleOwner, Observer { batches->
-                recyclerviewManageBatches.adapter = context?.let { BatchesAdapter(it,
-                    batches as ArrayList<Batch>
-                ) }
-            })
-
-            btnManageBatchCreateBatch.setOnClickListener {
-                createBatchDialog()
-            }
-
+        binding.btnManageBatchCreateBatch.setOnClickListener {
+            createBatchDialog()
         }
-        val mRecyclerView = binding.root.findViewById<RecyclerView>(R.id.recyclerview_manage_batches)
-        mRecyclerView.apply {
-            // set a LinearLayoutManager to handle Android
-            // RecyclerView behavior
-            layoutManager = LinearLayoutManager(activity)
-        }
+
         return binding.root
     }
 
@@ -90,7 +69,6 @@ class ManageBatchFragment : Fragment() {
         }
         createBtn.setOnClickListener {
             dialog.dismiss()
-            findNavController().navigate(ManageBatchFragmentDirections.actionManageBatchFragmentToBatchDetailsFragment())
             Toast.makeText(context,"ok", Toast.LENGTH_SHORT).show()
         }
     }
@@ -101,11 +79,22 @@ class ManageBatchFragment : Fragment() {
     }
 
     companion object {
-        @JvmStatic
-        fun onClick() {
-            Toast.makeText(context,"Batch clicked", Toast.LENGTH_SHORT).show()
-        }
+        @JvmField val ALPHABETICAL_COMPARATOR_BATCHES: Comparator<Batch> =
+            Comparator<Batch> { a: Batch, b: Batch -> a.trainerName!!.compareTo(b.trainerName!!) }
+    }
+
+    override fun onBatchClick(batchClicked: Batch) {
+        findNavController().navigate(ManageBatchFragmentDirections.actionManageBatchFragmentToBatchDetailsFragment(batchClicked))
+    }
+
+    private fun subscribeToViewModel() {
+        viewModel.batchesLiveData.observe(viewLifecycleOwner, Observer {
+            (binding.recyclerviewManageBatches.adapter as BatchAdapter).edit()
+                .replaceAll(it)
+                .commit()
+        })
     }
 }
+
 
 

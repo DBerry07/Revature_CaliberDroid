c802a6069f7f377498234e97441fef849b9e828c
novamapp <55009153+novamapp@users.noreply.github.com>
2020-03-06 00:33:47 -0500

    Created class for validation. Need to implement methods

---
+++ app/src/main/java/com/revature/caliberdroid/util/FieldValidator.java
@@ -0,0 +1,31 @@
+package com.revature.caliberdroid.util;
+
+import java.util.ArrayList;
+
+public class FieldValidator {
+
+    public static boolean isValidEmail(String input){
+        return false;
+    }
+
+    public static boolean isEmptyString(String input){
+        return false;
+    }
+
+    public static boolean isValidZipCode(String input){
+        return false;
+    }
+
+    public static boolean isValidTwoLetterState(ArrayList<String> statesList, String input){
+        return false;
+    }
+
+    public static boolean isValidState(ArrayList<String> stateList, String input){
+        return false;
+    }
+
+    //No special characters
+    public static boolean isValidPlainText(String input){
+        return false;
+    }
+}

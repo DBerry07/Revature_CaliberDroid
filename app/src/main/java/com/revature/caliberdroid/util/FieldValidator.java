012d3983128425a76078b25fa880c37ca604b23f
novamapp <55009153+novamapp@users.noreply.github.com>
2020-03-06 09:34:01 -0500

    Working on field validation

--- app/src/main/java/com/revature/caliberdroid/util/FieldValidator.java
+++ app/src/main/java/com/revature/caliberdroid/util/FieldValidator.java
@@ -1,18 +1,29 @@
 package com.revature.caliberdroid.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FieldValidator {
 
     public static boolean isValidEmail(String input){
+
         return false;
     }
 
     public static boolean isEmptyString(String input){
+        if(input == null) return true;
+        if(input.length() == 0) return true;
+        if(input.trim().length() == 0) return true;
         return false;
     }
 
     public static boolean isValidZipCode(String input){
+        String regex = "^[0-9]{5}(?:-[0-9]{4})?$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.matches()) return true;
         return false;
     }
 
@@ -28,4 +39,120 @@ public class FieldValidator {
     public static boolean isValidPlainText(String input){
         return false;
     }
+
+    ArrayList<String> TwoLetterStatesList = (ArrayList<String>) Arrays.asList(
+            "AL",
+            "AK",
+            "AS",
+            "AZ",
+            "AR",
+            "CA",
+            "CO",
+            "CT",
+            "DC",
+            "DE",
+            "FM",
+            "FL",
+            "GA",
+            "GU",
+            "HI",
+            "IA",
+            "ID",
+            "IL",
+            "IN",
+            "KS",
+            "LA",
+            "ME",
+            "MI",
+            "MN",
+            "MO",
+            "MP",
+            "MS",
+            "MT",
+            "NC",
+            "ND",
+            "NE",
+            "NH",
+            "NJ",
+            "NM",
+            "NV",
+            "NY",
+            "OH",
+            "OK",
+            "OR",
+            "PW",
+            "PA",
+            "PR",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VA",
+            "VI",
+            "VT",
+            "WA",
+            "WV",
+            "WI",
+            "WY"
+    );
+
+    ArrayList<String> StatesList = (ArrayList<String>) Arrays.asList(
+            "Alabama",
+            "Alaska",
+            "American Samoa",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "District of Columbia",
+            "Delaware",
+            "Federated States of Micronesia",
+            "Florida",
+            "Georgia",
+            "Guam",
+            "Hawaii",
+            "Iowa",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Kansas",
+            "Louisiana",
+            "Maine",
+            "Michigan",
+            "Minnesota",
+            "Missouri",
+            "Mississippi",
+            "Montana",
+            "Northern Mariana Islands",
+            "North Carolina",
+            "North Dakota",
+            "Nebraska",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "Nevada",
+            "New York",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Palau",
+            "Pennsylvania",
+            "Puerto Rico",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virgin Islands",
+            "VT",
+            "WA",
+            "WV",
+            "WI",
+            "WY"
+    );
 }

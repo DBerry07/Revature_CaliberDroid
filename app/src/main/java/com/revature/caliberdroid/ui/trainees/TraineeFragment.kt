b6c875ea4d55952cfeba9d5c1cbf44c075b9e79f
novamapp <55009153+novamapp@users.noreply.github.com>
2020-03-09 15:22:06 -0400

    Implemented getting the trainees based on selected batch. Corrected navigating back to TraineeFragment after AddTraineeFragment

--- app/src/main/java/com/revature/caliberdroid/ui/trainees/TraineeFragment.kt
+++ app/src/main/java/com/revature/caliberdroid/ui/trainees/TraineeFragment.kt
@@ -1,29 +1,23 @@
 package com.revature.caliberdroid.ui.trainees
 
-import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
-import com.revature.caliberdroid.R
-import com.revature.caliberdroid.data.api.TraineeAPIHandler
+import com.revature.caliberdroid.data.model.Batch
 import com.revature.caliberdroid.data.model.Trainee
-import com.revature.caliberdroid.data.repository.TraineeRepository.getTrainees
 import com.revature.caliberdroid.databinding.FragmentTraineeBinding
 import com.revature.revaturetraineemanagment.TraineeAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_trainee.view.*
+import timber.log.Timber
 
 /**
  * A simple [Fragment] subclass.
@@ -35,16 +29,23 @@ class TraineeFragment : Fragment() {
     private var _binding: FragmentTraineeBinding? = null
     private val binding get() = _binding!!
 
+    //Get the argument of select batch from BatchesInfoFragment
+    private val args: TraineeFragmentArgs by navArgs()
+    private lateinit var currentBatch: Batch
+
     private val model: TraineeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //Set the selected batch in order to make API call for trainees
+        currentBatch = args.BatchSelected
+        Timber.d("Selected batch: $currentBatch")
 
         _binding = FragmentTraineeBinding.inflate(layoutInflater, container, false)
         val view = binding.root
-        var batchId : Long = 50
+        var batchId : Long = currentBatch.batchID
         model.getTrainees(batchId)
         var traineeLayoutManager = LinearLayoutManager(view.context)
         var recyclerView = view.TM_recycler
@@ -71,7 +72,8 @@ class TraineeFragment : Fragment() {
 
         button.setOnClickListener {
             val navController = Navigation.findNavController(view)
-            navController.navigate(R.id.action_traineeFragment_to_addTraineeFragment)
+            //Pass currently selected branch to add trainee
+            navController.navigate(TraineeFragmentDirections.actionTraineeFragmentToAddTraineeFragment(currentBatch))
         }
 
         // Inflate the layout for this fragment

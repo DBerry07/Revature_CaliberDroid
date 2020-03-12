92043fabd5abdf3f17b84728e7aac468fb86b6de
novamapp <55009153+novamapp@users.noreply.github.com>
2020-03-12 15:19:06 -0400

    Completed Login UI

--- app/src/main/java/com/revature/caliberdroid/ui/LoginActivity.kt
+++ app/src/main/java/com/revature/caliberdroid/ui/LoginActivity.kt
@@ -1,13 +1,25 @@
 package com.revature.caliberdroid.ui
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.revature.caliberdroid.MainActivity
 import com.revature.caliberdroid.R
+import com.revature.caliberdroid.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.btnLogin.setOnClickListener {
+            val i:Intent = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+        setContentView(binding.root)
     }
 }

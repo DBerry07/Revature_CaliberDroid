5f5028f47233af9ae6b60148777a70925ba02fe1
Charles Mersereau <mersereau90@gmail.com>
2020-03-17 14:25:30 -0400

    button consistency

--- app/src/main/java/com/revature/caliberdroid/ui/qualityaudit/overall/QualityAuditOverallFragment.kt
+++ app/src/main/java/com/revature/caliberdroid/ui/qualityaudit/overall/QualityAuditOverallFragment.kt
@@ -15,6 +15,7 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.revature.caliberdroid.R
 import com.revature.caliberdroid.data.model.Category
 import com.revature.caliberdroid.data.model.SkillCategory
@@ -105,7 +106,7 @@ class QualityAuditOverallFragment : Fragment(), SkillCategoryAdapter.OnDeleteCli
 
     private fun showAddCategoriesDialog() {
 
-        val builder = AlertDialog.Builder(requireContext())
+        val builder = MaterialAlertDialogBuilder(requireContext())
         builder.setTitle(R.string.add_categories)
 
         val itemNames = viewModel.getActiveCategoryNames()
